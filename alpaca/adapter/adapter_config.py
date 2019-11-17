227f4b11975e507d666f1eed7ebf7296b649540a
horoiwa <horoiwa195@gmail.com>
2019-11-18 02:09:29 +0900

    Add adapter config

--- alpaca/adapter/adapter_config.py
+++ alpaca/adapter/adapter_config.py
@@ -1,19 +1,51 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List, Dict, Any
 
 
 @dataclass
 class AdapterConfig:
 
-    explainers: list = []
+    explainers: List[str] = field(default_factory=list)
 
-    objectives: list = []
+    objectives: List[str] = field(default_factory=list)
 
-    no_use_cols: list = []
+    no_use_cols: List[str] = field(default_factory=list)
 
-    constraint_max_min: dict = {}
+    constraint_max_min: Dict[str, List[int]] = field(default_factory=dict)
 
-    constraint_categorical: dict = {}
+    constraint_discrete: Dict[str, List[int]] = field(default_factory=dict)
 
-    constraint_discrete: dict = {}
+    constraint_categorical: Dict[str, List[Any]] = field(default_factory=dict)
 
-    constraint_sum_equal: dict = {}
+    constraint_sum_equal: List[Dict[int, List[str]]] = field(default_factory=list)
+
+    class Meta:
+        ordered = True
+
+
+if __name__ == "__main__":
+    import marshmallow_dataclass
+    import json
+    import os
+
+    AdapterSchema = marshmallow_dataclass.class_schema(AdapterConfig)
+    config = AdapterConfig()
+    config.constraint_max_min["varA"] = [10, 100]
+    config.constraint_categorical["varB"] = ["panda", 100]
+    config.constraint_sum_equal["varC"] = [{100, ["varD", "varE", "varF"]}]
+
+    config_json = AdapterSchema().dump(config)
+
+    print(config_json)
+    print(type(config_json))
+    if os.path.exists("example/test.json"):
+        os.remove("example/test.json")
+
+    with open("example/test.json", "w") as f:
+        json.dump(config_json, f, indent=4)
+
+    with open("example/test.json", "r") as f:
+        json_config = json.load(f)
+
+    config_2 = AdapterSchema().load(json_config)
+    print(config_2)
