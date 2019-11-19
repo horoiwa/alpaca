38bda96cddb468c82562ed7101ab3c3211555f7c
horoiwa <horoiwa195@gmail.com>
2019-11-20 01:30:13 +0900

    Add adapter test

--- alpaca/adapter/adapter.py
+++ alpaca/adapter/adapter.py
@@ -1,5 +1,8 @@
+import os
+import json
+
 import pandas as pd
-from alpaca.ml.config import AdapterConfig
+from alpaca.adapter.adapter_config import AdapterConfig
 
 
 class DataAdapter:
@@ -8,14 +11,40 @@ class DataAdapter:
         self.config = config if config else AdapterConfig()
 
     def fit(self, X, y):
-        pass
+        X, y = self._input_validation(X, y)
 
-    def save(self):
-        pass
+    def save(self, path_to_save=None):
+        path_to_save = path_to_save if path_to_save else "adapter_config.json"
+        if os.path.exists(path_to_save):
+            print(f"Overwritten Warning: {path_to_save} already exists")
+            os.remove(path_to_save)
+        with open(path_to_save, "w") as f:
+            config_json = self.config.to_json(indent=4, ensure_ascii=False)
+            config_json = json.loads(config_json)
+            json.dump(config_json, f)
+
+    def _input_validation(self, *args, **kwargs):
+        if len(args) >= 1:
+            X = args[0]
+            if isinstance(X, pd.Series):
+                X = pd.DataFrame(X).T
+            assert isinstance(X, pd.DataFrame), 'X must be DataFrame'
+
+            if len(args) >= 2:
+                y = args[1]
+                if isinstance(y, pd.Series):
+                    y = pd.DataFrame(y, columns=[y.name])
+                assert isinstance(y, pd.DataFrame), 'y must be DataFrame'
+                return X, y
+            else:
+                return X
+        else:
+            raise Exception("Unexpected input")
 
     @classmethod
-    def from_json(cls, adapter_config_path):
-        config = AdapterConfig.load_json(adapter_config_path)
+    def from_json(cls, path_to_config):
+        with open(path_to_config, "r") as f:
+            config = AdapterConfig.from_json(f.read())
         return cls(config)
 
 
---
+++ tests/test_adapter.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+
+from alpaca.adapter.adapter import DataAdapter
+
+
+class TestAdapter:
+
+    def setup_method(self):
+        self.adapter = DataAdapter()
+        self.tempdir = os.path.join("~", "temp")
+        self.path_to_save = os.path.join(self.tempdir, "adapter_config.json")
+        os.makedirs(self.tempdir)
+
+    def teardown_method(self):
+        del self.adapter
+        shutil.rmtree(self.tempdir)
+
+    def test_save_config(self):
+        self.adapter.save(self.path_to_save)
+
+    def test_load_confg(self):
+        self.adapter.config.explainers = ["A", "B", "C"]
+        self.adapter.config.objectives = ["D", "E"]
+        self.adapter.save(self.path_to_save)
+        adapter = DataAdapter.from_json(self.path_to_save)
+
+        assert adapter.config.explainers == ["A", "B", "C"]
+        assert adapter.config.objectives == ["D", "E"]
+
+
