ceac8cc88e24781cef35154a6b5cb7519d92fd9f
horoiwa <horoiwa195@gmail.com>
2020-01-10 00:37:09 +0900

    Refactor singlemodel

--- marmot/base_model.py
+++ marmot/base_model.py
@@ -1,7 +1,7 @@
 import warnings
 from abc import ABCMeta, abstractmethod
+import logging
 
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import sklearn
@@ -9,9 +9,10 @@ from sklearn.linear_model import ElasticNet, Lasso, Ridge
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import KFold, RepeatedKFold, train_test_split
 from sklearn.svm import SVR
-
 import optuna
 import xgboost as xgb
+
+from marmot.util import get_logger
 
 warnings.filterwarnings('ignore')
 
@@ -20,54 +21,54 @@ class BaseSingleModelCV(metaclass=ABCMeta):
 
     model_cls = None
 
-    def __init__(self, n_trials=200, scale=False, metric='mse',
-                 n_splits=3, n_repeats=3, silent=True):
+    def __init__(self, n_trials=200, metric='mse', scale=False,
+                 n_splits=3, n_repeats=10, silent=True, logger="base"):
+
         self.n_trials = n_trials
+
         self.metric = metric
+
+        self.scale = scale
+
         self.n_splits = n_splits
+
         self.n_repeats = n_repeats
 
+        self.silent = silent
+
+        self.logger = get_logger(logger)
+
     def fit(self, X, y):
+
         self.X, self.y = self._input_validation(X, y)
 
-        study = optuna.create_study(direction='minimize')
+        if self.silent:
+            optuna.logging.disable_default_handler()
+        self.logger.info("Start hyperparmeter optimization")
+
+        study = optuna.create_study(direction=self.direction)
         study.optimize(self, n_trials=self.n_trials)
         self.best_trial = study.best_trial
 
-        print()
-        print("Best score:", round(self.best_trial.value, 2))
-        print("Best params:", self.best_trial.params)
-        print()
+        self.logger.info(f"Best {self.metric} score: "
+                         + f"{round(self.best_trial.value, 2)}")
+        self.logger.info(f"Best hyperparams: "
+                         + f"{self.best_trial.params}")
+
 
         self.best_model = self.model_cls(**self.best_trial.params)
         self.best_model.fit(self.X, self.y)
 
     def predict(self, X):
         X = self._input_validation(X)
+        if self.scale:
+            raise NotImplementedError("scale")
         return self.best_model.predict(X)
 
     def score(self, X, y):
         X, y = self._input_validation(X, y)
 
         return self.best_model.score(X, y)
-
-    def valid(self, X, y, test_size=0.3):
-        X, y = self._input_validation(X, y)
-
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=test_size)
-        self.fit(X_train, y_train)
-
-        print("Tran R2 score", self.score(X_train, y_train))
-        print("Test R2 score", self.score(X_test, y_test))
-
-        pred_train = self.predict(X_train)
-        pred_test = self.predict(X_test)
-        plt.scatter(pred_train, y_train, label='train')
-        plt.scatter(pred_test, y_test, label='train')
-        plt.xlabel('predict')
-        plt.ylabel('observed')
-        plt.show()
 
     def kfold_cv(self, model):
         observed = []
@@ -85,29 +86,34 @@ class BaseSingleModelCV(metaclass=ABCMeta):
         if self.metric == 'mse':
             score = mean_squared_error(observed, predicted)
         elif self.metric == 'r2':
-            score = r2_score(observed, predicted) * -1
+            score = r2_score(observed, predicted)
         else:
             NotImplementedError('Unknown metric:', self.metric)
 
         return score
+
+    @property
+    def direction(self):
+        return ("minimize" if self.metric == "mse"
+                else "maximize" if self.metric == "r2"
+                else "Invalid metric")
 
     def _input_validation(self, *args, **kwargs):
         if len(args) >= 1:
             X = args[0]
             if isinstance(X, pd.Series):
                 X = pd.DataFrame(X).T
-            assert isinstance(X, pd.DataFrame), 'X must be DataFrame'
 
             if len(args) >= 2:
                 y = args[1]
                 if isinstance(y, pd.Series):
                     y = pd.DataFrame(y, columns=[y.name])
-                assert isinstance(y, pd.DataFrame), 'y must be DataFrame'
+
                 return X, y
             else:
                 return X
         else:
-            raise Exception("Unexpected input")
+            raise TypeError("Unexpected X or y")
 
     @abstractmethod
     def __call__(self, trial):
@@ -150,7 +156,7 @@ class LinearSVRCV(BaseSingleModelCV):
 
     def __call__(self, trial):
         C = trial.suggest_loguniform('C', 1e-2, 1e2)
-        epsilon = trial.suggest_loguniform('epsilon', 1e-3, 1e1)
+        epsilon = trial.suggest_loguniform('epsilon', 1e-3, 1e2)
         model = self.model_cls(kernel='linear', C=C,
                                epsilon=epsilon)
 
@@ -163,7 +169,7 @@ class KernelSVRCV(BaseSingleModelCV):
 
     def __call__(self, trial):
         C = trial.suggest_loguniform('C', 1e-2, 1e2)
-        epsilon = trial.suggest_loguniform('epsilon', 1e-3, 1e1)
+        epsilon = trial.suggest_loguniform('epsilon', 1e-3, 1e2)
         gamma = trial.suggest_loguniform('gamma', 1e-3, 1e3)
         model = self.model_cls(kernel='rbf', C=C,
                                epsilon=epsilon, gamma=gamma)
@@ -233,8 +239,9 @@ if __name__ == '__main__':
     X_sc = pd.DataFrame(StandardScaler().fit_transform(X), columns=X.columns)
     X_train, X_test, y_train, y_test = train_test_split(X_sc, y, test_size=0.3)
 
-    #model = LinearSVRCV(n_trials=10)
-    model = GBTRegCV(n_trials=10)
+    model = LinearSVRCV(n_trials=10)
+    #model = GBTRegCV(n_trials=10)
+    #model = KernelSVRCV(n_trials=50, metric="r2")
     model.fit(X_train, y_train)
 
     print(model.score(X_test, y_test))
